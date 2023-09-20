aae608eeeff1a4f3a2da313010510610cd5823d1
Dan Schulte <dan.schulte.qub@live.com>
2023-09-19 20:20:49 -0700

    Add Exceptions.UnwrapTo() and tests

---
+++ Everyone.Base.DotNet.Tests/ExceptionsTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Everyone
+{
+    public static class ExceptionsTests
+    {
+        public static void Test(TestRunner runner)
+        {
+            runner.TestType(typeof(Exceptions), () =>
+            {
+                runner.TestMethod("UnwrapTo<T>(object?)", () =>
+                {
+                    void UnwrapToTest<T>(object? value, T? expected)
+                    {
+                        runner.Test($"with {Language.AndList(new object?[] { typeof(T), value }.Map(runner.ToString))}", (Test test) =>
+                        {
+                            test.AssertEqual(expected, Exceptions.UnwrapTo<T>(value));
+                        });
+                    }
+
+                    UnwrapToTest<PreConditionFailure>(
+                        value: null,
+                        expected: null);
+                    UnwrapToTest<PreConditionFailure>(
+                        value: "hello",
+                        expected: null);
+                    UnwrapToTest<PreConditionFailure>(
+                        value: new[] { new PreConditionFailure("abc") },
+                        expected: null);
+                    UnwrapToTest<PreConditionFailure>(
+                        value: new PreConditionFailure("abc"),
+                        expected: new PreConditionFailure("abc"));
+                    UnwrapToTest<PreConditionFailure>(
+                        value: new AwaitException(new PreConditionFailure("abc")),
+                        expected: new PreConditionFailure("abc"));
+                    UnwrapToTest<PreConditionFailure>(
+                        value: new Exception("fake-message", new PreConditionFailure("abc")),
+                        expected: new PreConditionFailure("abc"));
+                    UnwrapToTest<PreConditionFailure>(
+                        value: UncaughtExceptionError.Create(new PreConditionFailure("abc")),
+                        expected: new PreConditionFailure("abc"));
+                    UnwrapToTest<PreConditionFailure>(
+                        value: new Exception("abc"),
+                        expected: null);
+                    UnwrapToTest<PreConditionFailure>(
+                        value: new AwaitException(new Exception("abc")),
+                        expected: null);
+                    UnwrapToTest<PreConditionFailure>(
+                        value: UncaughtExceptionError.Create(new Exception("abc")),
+                        expected: null);
+                });
+
+                runner.TestMethod("UnwrapTo(Type,object?)", () =>
+                {
+                    void UnwrapToTest(Type targetType, object? value, object? expected)
+                    {
+                        runner.Test($"with {Language.AndList(new object?[] { targetType, value }.Map(runner.ToString))}", (Test test) =>
+                        {
+                            test.AssertEqual(expected, Exceptions.UnwrapTo(targetType, value));
+                        });
+                    }
+
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: null,
+                        expected: null);
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: "hello",
+                        expected: null);
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: new[] { new PreConditionFailure("abc") },
+                        expected: null);
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: new PreConditionFailure("abc"),
+                        expected: new PreConditionFailure("abc"));
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: new AwaitException(new PreConditionFailure("abc")),
+                        expected: new PreConditionFailure("abc"));
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: new Exception("fake-message", new PreConditionFailure("abc")),
+                        expected: new PreConditionFailure("abc"));
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: UncaughtExceptionError.Create(new PreConditionFailure("abc")),
+                        expected: new PreConditionFailure("abc"));
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: new Exception("abc"),
+                        expected: null);
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: new AwaitException(new Exception("abc")),
+                        expected: null);
+                    UnwrapToTest(
+                        targetType: typeof(PreConditionFailure),
+                        value: UncaughtExceptionError.Create(new Exception("abc")),
+                        expected: null);
+                });
+            });
+        }
+    }
+}
---
+++ Everyone.Base.DotNet/Exceptions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Everyone
+{
+    public static class Exceptions
+    {
+        /// <summary>
+        /// Unwrap the provided <paramref name="value"/> to the provided type
+        /// <typeparamref name="T"/>. This will unwrap any <see cref="Exception"/> and
+        /// <see cref="UncaughtExceptionError"/> until it runs out of values to unwrap and returns
+        /// null or finds the type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type to unwrap the <paramref name="value"/> to.</typeparam>
+        /// <param name="value">The value to unwrap.</param>
+        public static T? UnwrapTo<T>(object? value)
+        {
+            return (T?)Exceptions.UnwrapTo(typeof(T), value);
+        }
+
+        /// <summary>
+        /// Unwrap the provided <paramref name="value"/> to the provided
+        /// <paramref name="targetType"/>. This will unwrap any <see cref="Exception"/> and
+        /// <see cref="UncaughtExceptionError"/> until it runs out of values to unwrap and returns
+        /// null or finds the <paramref name="targetType"/>.
+        /// </summary>
+        /// <param name="targetType">The type to unwrap the <paramref name="value"/> to.</param>
+        /// <param name="value">The value to unwrap.</param>
+        public static object? UnwrapTo(Type targetType, object? value)
+        {
+            Pre.Condition.AssertNotNull(targetType, nameof(targetType));
+
+            object? result = value;
+            while (true)
+            {
+                if (result?.GetType() == targetType)
+                {
+                    break;
+                }
+                else if (result is Exception resultException)
+                {
+                    result = resultException.InnerException;
+                }
+                else if (result is UncaughtExceptionError resultError)
+                {
+                    result = resultError.UncaughtException;
+                }
+                else
+                {
+                    result = null;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
